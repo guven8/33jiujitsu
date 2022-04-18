d6afbbffa6ce25d7bb80ab3ce132b851d916cd15
guven8 <guvenkemal86@gmail.com>
2022-04-18 14:28:24 +0100

    set next config to default

--- next.config.js
+++ next.config.js
@@ -2,13 +2,7 @@ const debug = process.env.NODE_ENV !== 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' }
-    };
-  },
-  assetPrefix: !debug ? 'https://33jiujitsu.co.uk/' : ''
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
