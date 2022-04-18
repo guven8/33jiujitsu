b2fd2957da5cbbb666de2a602672ac2781e4a45b
guven8 <guvenkemal86@gmail.com>
2022-04-18 12:37:39 +0100

    add asset prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: '/33jiujitsu/'
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
